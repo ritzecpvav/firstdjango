e66fd6aa8bfd6ba15393a4d5436ae4e7913c7678
Ritz Ghever Libetario <yahshua.ecpvav@gmail.com>
2021-09-08 17:09:29 +0800

    update again

--- mysite/local_setting.py
+++ mysite/local_setting.py
@@ -3,8 +3,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'firstdjango',
+        'USER': 'ritz',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
